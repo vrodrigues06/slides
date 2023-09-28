5af7d7c3a6305b73ad38ef02ff639772fb35f2bc
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-09-28 12:01:30 -0300

    add custom-controls e estilos

--- js/slide.js
+++ js/slide.js
@@ -154,7 +154,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(container, slide) {
     super(container, slide);
     this.bindControlEvents();
